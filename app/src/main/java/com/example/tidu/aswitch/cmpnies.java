3707b79e98edb664c9f1cdd5454e87222a331915
uditDasari <uditdasari19@gmail.com>
2019-11-25 00:25:15 +0530

    .

--- app/src/main/java/com/example/tidu/aswitch/cmpnies.java
+++ app/src/main/java/com/example/tidu/aswitch/cmpnies.java
@@ -3,20 +3,21 @@ package com.example.tidu.aswitch;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.ImageView;
 
 public class cmpnies extends AppCompatActivity {
 
-    ImageView imageView,imageView1,imageView2,imageView3;
+    CardView imageView,imageView1,imageView2,imageView3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cmpnies);
-        /*imageView = findViewById(R.id.imageView4);
-        imageView1 = findViewById(R.id.imageView5);
-        imageView2=findViewById(R.id.imageView6);
-        imageView3=findViewById(R.id.imageView8);
+        imageView = findViewById(R.id.cardView4);
+        imageView1 = findViewById(R.id.cardView);
+        imageView2=findViewById(R.id.cardView3);
+        imageView3=findViewById(R.id.cardView2);
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,6 +41,6 @@ public class cmpnies extends AppCompatActivity {
             public void onClick(View v) {
                 startActivity(new Intent(cmpnies.this,Grubhub.class));
             }
-        });*/
+        });
     }
 }
