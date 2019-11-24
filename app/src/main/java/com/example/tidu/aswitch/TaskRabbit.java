5f426ee24ecf4b2756cdab7b053b390f6ca8edd9
Rajprasaddora <rajdora06802225202@gmail.com>
2019-11-24 21:15:39 +0530

    ..

---
+++ app/src/main/java/com/example/tidu/aswitch/TaskRabbit.java
@@ -0,0 +1,13 @@
+package com.example.tidu.aswitch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TaskRabbit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_rabbit);
+    }
+}
