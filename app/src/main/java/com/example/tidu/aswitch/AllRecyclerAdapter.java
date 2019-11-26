46085e6958dd0ac442b43db1922c194561e09aa2
uditDasari <uditdasari19@gmail.com>
2019-11-26 21:35:08 +0530

    ..

--- app/src/main/java/com/example/tidu/aswitch/AllRecyclerAdapter.java
+++ app/src/main/java/com/example/tidu/aswitch/AllRecyclerAdapter.java
@@ -10,15 +10,23 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class AllRecyclerAdapter extends RecyclerView.Adapter<AllRecyclerAdapter.AllViewHolder> {
 
     Context context;
+    ArrayList<POJO> arrayList;
+
+    public AllRecyclerAdapter(Context context, ArrayList<POJO> arrayList) {
+        this.context = context;
+        this.arrayList = arrayList;
+    }
 
     @NonNull
     @Override
     public AllViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from()
-        return null;
+        View view= LayoutInflater.from(context).inflate()
+        return AllViewHolder(view);
     }
 
     @Override
@@ -28,7 +36,7 @@ public class AllRecyclerAdapter extends RecyclerView.Adapter<AllRecyclerAdapter.
 
     @Override
     public int getItemCount() {
-        return 0;
+        return arrayList.size();
     }
 
     public  class AllViewHolder extends RecyclerView.ViewHolder
@@ -37,6 +45,7 @@ public class AllRecyclerAdapter extends RecyclerView.Adapter<AllRecyclerAdapter.
         TextView textView;
         public AllViewHolder(@NonNull View itemView) {
             super(itemView);
+
         }
     }
 }
