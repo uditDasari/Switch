5e8c698a52122706c3c4dfb6ec1ca312c6da4a44
uditDasari <uditdasari19@gmail.com>
2019-11-27 23:58:15 +0530

    .

--- app/src/main/java/com/example/tidu/aswitch/lyftDetail.java
+++ app/src/main/java/com/example/tidu/aswitch/lyftDetail.java
@@ -3,12 +3,22 @@ package com.example.tidu.aswitch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class lyftDetail extends AppCompatActivity {
 
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lyft_detail);
+        setContentView(R.layout.fragment_graph);
+        imageView=findViewById(R.id.imageView15);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
