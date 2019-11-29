f4d934616d4498b14eec34aab44415536520eb95
uditDasari <uditdasari19@gmail.com>
2019-11-29 16:26:56 +0530

    .

--- app/src/main/java/com/example/tidu/aswitch/dollarFragment.java
+++ app/src/main/java/com/example/tidu/aswitch/dollarFragment.java
@@ -1,20 +1,33 @@
 package com.example.tidu.aswitch;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.google.android.material.tabs.TabLayout;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class dollarFragment extends Fragment {
-
+    TabLayout tabLayout,tabLayout1;
+    DisplayMetrics displaymetrics ;
+    ViewPagerAdapter pagerAdapter;
+    ViewPager viewPager;
+    CardView cardView;
 
     public dollarFragment() {
         // Required empty public constructor
@@ -27,5 +40,44 @@ public class dollarFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dollar, container, false);
     }
-
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        // super.onViewCreated(view, savedInstanceState);
+        tabLayout=getView().findViewById(R.id.tabsdollar2);
+        viewPager=getView().findViewById(R.id.view_pager_id_dollar);
+        pagerAdapter=new ViewPagerAdapter(getFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+        tabLayout1=getView().findViewById(R.id.tabsdollar1);
+        for(int i=0; i < tabLayout1.getTabCount(); i++) {
+            View tab = ((ViewGroup) tabLayout1.getChildAt(0)).getChildAt(i);
+            ViewGroup.MarginLayoutParams p = (ViewGroup.MarginLayoutParams) tab.getLayoutParams();
+            p.setMargins(0, 0, 50, 0);
+            tab.requestLayout();
+        }
+        cardView=getView().findViewById(R.id.carddetaildollar);
+        /*cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(),lyftDetail.class));
+            }
+        });*/
+        tabLayout.setupWithViewPager(viewPager);
+        //getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        LinearLayout layout = ((LinearLayout) ((LinearLayout) tabLayout.getChildAt(0)).getChildAt(0));
+        LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) layout.getLayoutParams();
+        layoutParams.weight = 0.18f;
+        layout.setLayoutParams(layoutParams);
+        LinearLayout layout1 = ((LinearLayout) ((LinearLayout) tabLayout.getChildAt(0)).getChildAt(1));
+        LinearLayout.LayoutParams layoutParams1 = (LinearLayout.LayoutParams) layout1.getLayoutParams();
+        layoutParams1.weight = 0.488f;
+        layout1.setLayoutParams(layoutParams1);
+        LinearLayout layout2 = ((LinearLayout) ((LinearLayout) tabLayout.getChildAt(0)).getChildAt(2));
+        LinearLayout.LayoutParams layoutParams2 = (LinearLayout.LayoutParams) layout2.getLayoutParams();
+        layoutParams2.weight = 0.286f;
+        layout2.setLayoutParams(layoutParams2);
+        LinearLayout layout3 = ((LinearLayout) ((LinearLayout) tabLayout.getChildAt(0)).getChildAt(3));
+        LinearLayout.LayoutParams layoutParams3 = (LinearLayout.LayoutParams) layout3.getLayoutParams();
+        layoutParams3.weight = 0.257f;
+        layout3.setLayoutParams(layoutParams3);
+    }
 }
